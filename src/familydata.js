43dfa663a39f23ac07c4267780d9385ecb8d9bd0
Marco Bihrle <bihrle.marco@hotmail.com>
2021-07-18 13:53:33 +0200

    fixing security issues updated libraries

---
+++ src/familydata.js
@@ -0,0 +1,74 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Sebastian Bihrle",
+        username: "Sebastian",
+        email: "bihrle.sebastia@gmail.com",
+    },
+    {
+        id: 2,
+        name: "Benjamin Bihrle",
+        username: "Benjamin",
+        email: "bihrle.sebastia@gmail.com",
+    },
+    {
+        id: 3,
+        name: "Maren Bihrle",
+        username: "Maren",
+        email: "email@email.com",
+    },
+    {
+        id: 4,
+        name: "Marco Bihrle",
+        username: "Marco",
+        email: "email@email.com",
+    },
+    {
+        id: 5,
+        name: "Michelle Fähler",
+        username: "Michelle",
+        email: "email@email.com",
+    },
+    {
+        id: 6,
+        name: "Saskia Fähler",
+        username: "Saskia",
+        email: "email@email.com",
+    },
+    {
+        id: 7,
+        name: "Stefan Fähler",
+        username: "Stefan",
+        email: "email@email.com",
+    },
+    {
+        id: 8,
+        name: "Nessi Deffner",
+        username: "Nessi",
+        email: "email@email.com",
+    },
+    {
+        id: 9,
+        name: "Oma Angelika",
+        username: "Angelika",
+        email: "email@email.com",
+    },
+    {
+        id: 10,
+        name: "Opa Frank",
+        username: "Frank",
+        email: "email@email.com",
+    },
+    {
+        id: 11,
+        name: "Oma Brigitte",
+        username: "Brigitte",
+        email: "email@email.com",
+    },
+    {
+        id: 12,
+        name: "Opa Jürgen",
+        username: "Jürgen",
+        email: "email@email.com",
+    },
+];
